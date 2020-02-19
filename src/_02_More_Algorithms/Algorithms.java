f50aa7bdcd0f223150386fd9b7fb6021c8accb70
ilabstaff <ilabstaff@172.16.116.128>
2020-02-18 17:50:35 -0800

    working on the module 2 checkpoint

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -8,35 +8,33 @@ import _00_Sorting_Algorithms._00_SortedArrayChecker;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-	
-			for (int i = 0; i < eggs.size(); i++) {
-				if (eggs.get(i).equals("cracked")) {
-					
-					return i;
-				}
-			
-		
-		
-	}
-			return 0;
+
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+
+				return i;
+			}
+
+		}
+		return 0;
 	}
 
 	public static int countPearls(List<Boolean> oysters) {
-		int count=0;
+		int count = 0;
 		for (int i = 0; i < oysters.size(); i++) {
-			if (oysters.get(i)==true) {
-				
-				count+=1;
+			if (oysters.get(i) == true) {
+
+				count += 1;
 			}
 		}
 		return count;
 	}
 
 	public static double findTallest(List<Double> peeps) {
-		double count=0;
+		double count = 0;
 		for (int i = 0; i < peeps.size(); i++) {
-			if (peeps.get(i)>count) {
-				count=peeps.get(i);
+			if (peeps.get(i) > count) {
+				count = peeps.get(i);
 			}
 		}
 		return count;
@@ -44,11 +42,11 @@ public class Algorithms {
 
 	public static String findLongestWord(List<String> words) {
 		String Word = null;
-		int word=words.get(0).length();
+		int word = words.get(0).length();
 		for (int i = 0; i < words.size(); i++) {
-			if (words.get(i).length()>word) {
-				word=words.get(i).length();
-				Word=words.get(i);
+			if (words.get(i).length() > word) {
+				word = words.get(i).length();
+				Word = words.get(i);
 			}
 		}
 		return Word;
@@ -63,36 +61,48 @@ public class Algorithms {
 		return false;
 	}
 
-	public static List<String> sortScores(List<Double> results) {
-		double space=0;
+	public static List<Double> sortScores(List<Double> results) {
+		double space = 0;
 		for (int j = 0; j < results.size(); j++) {
-		for (int i = j+1; i < results.size(); i++) {
-			if (results.get(j)>results.get(i)) {
-				space=results.get(j);
-				results.set(j, results.get(i));
-				results.set(i, space);
-			}
-		}}
-		
-		return null;
-}
-	public static List <Integer> Dna(List<String> unsortedSequences){
-		String space=null; 
-		for (int i = 0; i < unsortedSequences.size(); i++) {
-			for (int j = 0; j < unsortedSequences.size(); j++) {
-				if (unsortedSequences.get(i).length()>unsortedSequences.get(i+1).length()) {
-					space=unsortedSequences.get(i);
-					unsortedSequences.set(i,unsortedSequences.get(j));
-					unsortedSequences.set(j, space);
+			for (int i = j + 1; i < results.size(); i++) {
+				if (results.get(j) > results.get(i)) {
+					space = results.get(j);
+					results.set(j, results.get(i));
+					results.set(i, space);
 				}
 			}
 		}
-		
-		return null;
+
+		return results;
 	}
-	public static List <String>sortword(List <String> words){
-		
+
+	public static List<String> sortDNA(List<String> unsortedSequences) {
+		String space = null;
+		for (int j = 0; j < unsortedSequences.size(); j++) {
+			for (int i =j+1; i < unsortedSequences.size(); i++) {
+				if (unsortedSequences.get(j).length() > unsortedSequences.get(i).length()) {
+					space = unsortedSequences.get(j);
+					unsortedSequences.set(j, unsortedSequences.get(i));
+					unsortedSequences.set(i, space);
+				}
+			}
+		}
+
+		return unsortedSequences;
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		String space = null;
+			for (int j = 0; j < words.size(); j++) {
+				for (int i = j+1; i < words.size(); i++) {
+				int W = words.get(i).compareTo(words.get(j));
+				if (W < 0) {
+					space = words.get(j);
+					words.set(j, words.get(i));
+					words.set(i, space);
+				}
+			}
+		}
+		return words;
 	}
 }
-
-
---
+++ src/_02_More_Algorithms/Checkpoint.java
@@ -0,0 +1,14 @@
+package _02_More_Algorithms;
+
+public class Checkpoint {
+	double[] Double[];
+public static void main(String[] args) {
+	
+}
+
+public Double sort( Double[] dub) {
+	
+	return null;
+	
+}
+}
